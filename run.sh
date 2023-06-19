152a5348c219a2419488dc24f1921f66bcc53071
Yoav Karpassi <yoavkarp@gmail.com>
2023-06-19 11:20:57 +0300

    .

--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-
-docker run -d --name my_nginx_container -p 80:80 nginx:latest
--- stop.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-docker stop my_nginx_container || true
-docker rm my_nginx_container || true
